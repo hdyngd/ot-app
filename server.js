4258fe281ab54aeb7ac7f71d3edc0743d81ef7a2
柳田英彦 <hide@satouhidehiko-no-MacBook-Air-2.local>
2019-05-07 09:20:15 +0900

    first commit

---
+++ server.js
@@ -0,0 +1,18 @@
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/index.html');
+});
+
+http.listen(3000, function(){
+  console.log('listening on *:3000');
+});
+
+var EditorSocketIOServer = require('ot/lib/editor-socketio-server');
+var server = new EditorSocketIOServer("", [], 1);
+
+io.on('connection', function(socket){
+  server.addClient(socket);
+});
